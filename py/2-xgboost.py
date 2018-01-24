b8d29a0d0d57bb23855cad9aee52169e6640510f
szilard <szilard@users.noreply.github.com>
2018-01-24 14:04:23 -0800

    py dummy enc

--- py/2-xgboost.py
+++ py/2-xgboost.py
@@ -11,12 +11,16 @@ import multiprocessing
 
 d = pd.read_csv("../data/airline100K.csv")
 
-X = d.drop("dep_delayed_15min", 1)
-
-categorical_names = ["Month" , "DayofMonth" , "DayOfWeek" , "UniqueCarrier" , "Origin" , "Dest" ]
-for name in categorical_names:
-    le = LabelEncoder().fit(X[name])
-    X[name]  = le.transform(X[name])
+vars_cat = ["Month","DayofMonth","DayOfWeek","UniqueCarrier", "Origin", "Dest"]
+vars_num = ["DepTime","Distance"]
+def my_get_dummies(d, col):
+    dd = pd.get_dummies(d.ix[:, col])
+    dd.columns = [col + "_%s" % c for c in dd.columns]
+    return(dd)
+    
+X_cat = pd.concat([my_get_dummies(d, col) for col in vars_cat], axis = 1)
+X = pd.concat([X_cat, d.ix[:,vars_num]], axis = 1)    
+## TODO: training very slow, needs sparse representation!
       
 y = np.where(d[["dep_delayed_15min"]]=="Y",1,0)[:,0]
 
