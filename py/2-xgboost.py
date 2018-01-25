1c1b800165b1324ee29f5332d3393c857aec556e
szilard <szilard@users.noreply.github.com>
2018-01-25 06:18:44 -0800

    bugs

--- py/2-xgboost.py
+++ py/2-xgboost.py
@@ -25,30 +25,40 @@ y = np.where(d["dep_delayed_15min"]=="Y",1,0)                        # numpy arr
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1, random_state=123)
 
 
-## TRAIN method 1 - sklearn API
+
+## Method 1 - sklearn API
+
+## TRAIN
 md = xgb.XGBClassifier(max_depth=10, n_estimators=100, learning_rate=0.1, n_jobs=-1)
 %time md.fit(X_train, y_train)
 
 ## %time md.fit(X_train.toarray(), y_train)   # slow if not sparse!
 
 
-## TRAIN method 2 - orig xgboost API
-dxgb_train = xgb.DMatrix(X_train, label = y_train)
-param = {'max_depth':10, 'eta':0.1, 'objective':'binary:logistic', 'silent':1, 'nthread': multiprocessing.cpu_count()}
-%time md = xgb.train(param, dxgb_train, num_boost_round = 100)
-
-
-
 ## SCORE
 y_pred = md.predict_proba(X_test)[:,1]
 
 metrics.confusion_matrix(y_test, y_pred>0.5)
-
 metrics.roc_auc_score(y_test, y_pred)
 
-metrics.confusion_matrix(y_test, y_pred>0.7)
-metrics.confusion_matrix(y_test, y_pred>0.6)
+
+
+## Method 2 - orig xgboost API
+
+dxgb_train = xgb.DMatrix(X_train, label = y_train)
+dxgb_test = xgb.DMatrix(X_test)
+
+
+## TRAIN
+param = {'max_depth':10, 'eta':0.1, 'objective':'binary:logistic', 
+             'silent':1, 'nthread': multiprocessing.cpu_count()}
+%time md = xgb.train(param, dxgb_train, num_boost_round = 100)
+
+## SCORE
+y_pred = md.predict(dxgb_test)   
+
 metrics.confusion_matrix(y_test, y_pred>0.5)
+metrics.roc_auc_score(y_test, y_pred)
 
 
 ## try playing with the hyperparams e.g. max_depth = 2,5,10,15; learning_rate=0.01,0.03,0.1;
